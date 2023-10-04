205b838a65ca4da3929f0c2a4e307a36a80462a5
lis <ElisabetSVick@outlook.dk>
2023-10-04 11:25:04 +0200

    setup

---
+++ activate_kernel.sh
@@ -0,0 +1,2 @@
+source ./nlp_env/bin/activate
+python -m ipykernel install --user --name=nlp_env
---
+++ setup_env.sh
@@ -0,0 +1,8 @@
+# create env uisng venv
+python -m venv nlp_env
+
+# activate env
+source ./nlp_env/bin/activate
+
+# install requirements
+python -m pip install -r requirements.txt
